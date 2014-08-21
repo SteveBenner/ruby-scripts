946183e6045a63525f33e8f2eff81e88766c8553
Stephen Benner <sbenner9@gmail.com>
2014-08-21 13:38:23 -0700

    updated lolping
    Signed-off-by: Stephen Benner <sbenner9@gmail.com>

--- fun/lolping.rb
+++ fun/lolping.rb
@@ -1,36 +1,43 @@
 #!/usr/bin/env ruby
 # League of Legends network tools
 #
+# $1: number of pings (one per second)
+#
 require 'colorize'
 
-# todo: thread
+color_lag = lambda do |ping|
+	lag_color = case ping
+		when 0..149 then :green
+		when 150..299 then :yellow
+		else :red
+	end
+	ping.to_s.colorize color: lag_color
+end
 
-# Data
-SERVERS = [
-	'216.52.241.254',
-	'66.151.54.190',
-	'50.203.115.149', # NA comcast server
-	'66.150.148.1',   # NA, from: http://forums.na.leagueoflegends.com/board/showthread.php?t=3134222
-	'64.7.194.1'      #
-]
+# Data (source: http://forums.na.leagueoflegends.com/board/showthread.php?t=3134222)
+SERVERS = %w[216.52.241.254 66.151.54.190 50.203.115.149 66.150.148.1 64.7.194.1]
+
 # this is just for nice even terminal output...
 longest_ip = SERVERS.max_by { |s| s.length }
 SERVERS.collect! { |s| s << ' '*(longest_ip.length - s.length) }
 
-DEFAULT_LENGTH = 30 # number of seconds (1 ping per second)
-PING_COUNT = ARGV.first || DEFAULT_LENGTH
-puts "Pinging servers #{PING_COUNT.white} times using #{SERVERS.count.white} threads..."
+lowest, highest = [], []
+DEFAULT_NUM_PINGS = 30
+PING_COUNT = ARGV.first || DEFAULT_NUM_PINGS
+
+puts "Pinging servers #{PING_COUNT.to_s.white} times using #{SERVERS.count.to_s.white} threads..."
+
 threads = SERVERS.collect do |server|
   Thread.new(server) do |server|
-  	results = `ping -c #{PING_COUNT} #{server}`.scan(/time=(\d*\.\d*)/)
-  	avg_latency = results.flatten.map(&:to_f).reduce(:+) / results.count.to_f
-  	color = case avg_latency
-      when 0..200 then :cyan
-      when 201..300 then :yellow
-      else :red
-    end
-  	puts "Pinging #{server.magenta} #{PING_COUNT.to_s.white} times resulted in "\
-  			 "average latency of: #{avg_latency.to_s.colorize(color: color)}"
+	  results     = `ping -c #{PING_COUNT} #{server}`.scan(/time=(\d*\.\d*)/).flatten.map! &:to_f
+	  avg_latency = results.reduce(:+) / results.count.to_f
+	  lowest     << results.min
+	  highest    << results.max
+	  puts 'Pinging '.white + server.light_blue + ' ' + PING_COUNT.to_s.light_yellow +
+					' times resulted in average latency of: '.white + color_lag.call(avg_latency)
   end
 end
 threads.map &:join
+
+puts 'Your lowest recorded ping was '.white + color_lag.call(lowest.min) +
+	   ' and your highest was '.white + color_lag.call(highest.max) + '.'
