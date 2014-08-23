737a6051a5fe2dc429c4b9c1e771434d4d2d1b5f
Stephen Benner <sbenner9@gmail.com>
2014-08-23 09:54:12 -0700

    Added post-commit hook for converting tabs to spaces.

---
+++ git-hooks/post-commit/convert-tabs-to-spaces.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+#
+# This script converts all tabs into spaces in all the files
+# within the current git repository, as defined by TAB_SIZES.
+#
+TAB_SIZES = { rb: 2, coffee: 2, sass: 2, slim: 2, js: 4 }
+# Dir.glob("../../**/*.{#{TAB_SIZES.keys.join(',')}") do |file|
+EXTENSIONS = TAB_SIZES.keys.map { |ext| ".#{ext.to_s}" }
+`git ls-files`.split.select { |f| EXTENSIONS.include? File.extname(f) }.each do |file|
+	path = File.expand_path(file)
+	space_str = ' '*TAB_SIZES[File.extname(file)[1..-1].to_sym]
+	File.write path, File.open(path) { |f| f.read.gsub(/\t/, space_str) }
+end
