97f629c22e07bf9e3527a96d0116151d81c6b2a2
Stephen Benner <sbenner9@gmail.com>
2014-11-20 16:18:14 -0800

    fast commit

--- git/git-hooks/post-commit/convert-tabs-to-spaces.rb
+++ git/git-hooks/post-commit/convert-tabs-to-spaces.rb
@@ -7,6 +7,6 @@ TAB_SIZES = { rb: 2, coffee: 2, sass: 2, slim: 2, js: 4 }
 EXTENSIONS = TAB_SIZES.keys.map { |ext| ".#{ext.to_s}" }
 `git ls-files --full-name`.split.select { |f| EXTENSIONS.include? File.extname(f) }.each do |file|
 	path = File.expand_path(file)
-	space_str = ' '*TAB_SIZES[File.extname(file)[1..-1].to_sym]
+	space_str = ' ' * TAB_SIZES[File.extname(file)[1..-1].to_sym]
 	File.write path, File.open(path) { |f| f.read.gsub(/\t/, space_str) }
 end
