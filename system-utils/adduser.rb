3f58e70c8b6e38a9fe4203fe8b3ae0d2a345d812
Stephen Benner <sbenner9@gmail.com>
2015-05-17 07:11:48 -0700

    added `adduser` cli script

--- system-utils/adduser.rb
+++ system-utils/adduser.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# CLI tool for adding a user to your OS X system
+# CLI tool for adding a user to an OS X system
 # Wraps Directory Services CLI tool `dscl`
 # 
 # @author Stephen Benner
@@ -10,15 +10,21 @@ require 'optparse'
 
 # Default options
 opts = {
-	
+  
 }
 # Command line interface
 optparser = OptionParser.new do |cli|
   cli.version = '0.0.1'
-	cli.summary_width  = 24
-	cli.summary_indent = ' ' * 2
-	cli.banner = 'CLI tool for adding a user to your OS X system'
-	# OPTIONS GO HERE
-	cli.on_tail('-h', '--help', '--usage', 'Display this message.') { puts cli; exit }
+  cli.summary_width  = 24
+  cli.summary_indent = ' ' * 2
+  cli.banner = 'CLI tool for adding a user to your OS X system' + $/ +
+	  'Usage: adduser [options] USERNAME'
+  cli.on('-f', '--full-name', 'Full name for user') { |name| opts[:RealName] = name }
+  cli.on('-s', '--shell', 'Shell for user') { |path| opts[:UserShell] = path }
+  cli.on('-i', '--id', 'UniqueID of user (must be unique)') { |id| opts[:UniqueID] = id }
+  cli.on('-g', '--group-id', 'Group ID of user') { |id| opts[:PrimaryGroupID] = id }
+  cli.on('-a', '--admin', 'Makes the user an administrator') { opts[:admin] = true}
+  cli.on('-p', '--password', 'Password for user') { |pw| opts[:password] = pw}
+  cli.on_tail('-h', '--help', '--usage', 'Display this message.') { puts cli; exit }
   cli.on_tail('--version', 'Display script version.') { puts cli.version; exit }
 end.parse!
