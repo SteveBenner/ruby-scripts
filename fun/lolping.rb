5212251406093de6c908bca91cdf8bd112df885f
Stephen Benner <sbenner9@gmail.com>
2017-05-24 17:33:12 -0700

    update to `lolping`

--- fun/lolping.rb
+++ fun/lolping.rb
@@ -15,7 +15,7 @@ color_lag = lambda do |ping|
 end
 
 # Data (source: http://forums.na.leagueoflegends.com/board/showthread.php?t=3134222)
-SERVERS = %w[216.52.241.254 66.151.54.190 50.203.115.149 66.150.148.1 64.7.194.1]
+SERVERS = %w[104.160.131.3 104.160.131.1 216.52.241.254 66.151.54.190 50.203.115.149 66.150.148.1]
 
 # this is just for nice even terminal output...
 longest_ip = SERVERS.max_by { |s| s.length }
@@ -27,10 +27,11 @@ PING_COUNT = ARGV.first || DEFAULT_NUM_PINGS
 
 puts "Pinging #{SERVERS.count.to_s.white} servers concurrently, for #{PING_COUNT.to_s.white} seconds..."
 
+# todo: figure out why this throws an ArgumentError for some reason, claiming `results` is Nil for the last thread
 threads = SERVERS.collect do |server|
   Thread.new(server) do |server|
     begin
-      results   = `ping -c #{PING_COUNT} #{server}`.scan(/time=(\d*\.\d*)/).flatten.map! &:to_f
+      results = `ping -c #{PING_COUNT} #{server}`.scan(/time=(\d*\.\d*)/).flatten.map! &:to_f
     rescue
       abort 'ERROR: Unable to form a connection; there may be a problem with your network.'
     end
@@ -41,7 +42,7 @@ threads = SERVERS.collect do |server|
           ' times resulted in average latency of: '.white + color_lag.call(avg_latency)
   end
 end
-threads.map &:join
+threads.each &:join
 
 puts 'Your lowest recorded ping was '.white + color_lag.call(lowest.min) +
      ' and your highest was '.white + color_lag.call(highest.max) + '.'
